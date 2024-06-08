f18952c742adccd40ee6b9ae580aae8b6edaedf3
T Mohamed Yaser <140265777+Yaser-123@users.noreply.github.com>
2024-06-08 20:12:35 +0530

    Update effect.js
    Enabled smooth scrolling and advanced navigation bar

--- effect.js
+++ effect.js
@@ -76,9 +76,9 @@ modeToggleBtn.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
     body.classList.toggle('light-mode');
     if (body.classList.contains('dark-mode')) {
-        modeToggleBtn.textContent = 'Light Mode';
+        modeToggleBtn.textContent = 'Light';
     } else {
-        modeToggleBtn.textContent = 'Dark Mode';
+        modeToggleBtn.textContent = 'Dark';
     }
 });
 
@@ -114,5 +114,23 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwJNF-_VCLXC1a3EOGmVGvEDThTt-6gBxSsdEniVQyD0IYgYx7Pp7vbOp5AeHnHkzKN0g/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },2000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
+
 
     
